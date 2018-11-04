32d1cebea0c3cc4db04e7b62f4a2fb4c3f8f951c
zqhZY <563078785@qq.com>
2018-11-04 22:04:14 +0800

    add esim

---
+++ mains/sent_sem_esim.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+import sys
+
+sys.path.extend(['..'])
+
+from data_loader.sent_sem_loader import SemDataGenerator, MSRPGenerator, ATECGenerator
+from models.esim import ESIM
+from trainers.sent_sem_trainer import SentSemTrainer
+from utils.config import process_config
+from utils.dirs import create_dirs
+from utils.logger import Logger
+from utils.utils import get_args
+
+
+def main():
+    # capture the config path from the run arguments
+    # then process the json configuration file
+    try:
+        args = get_args()
+        config = process_config(args.config)
+
+    except Exception as e:
+        print("missing or invalid arguments %s" % e)
+        exit(0)
+
+    # create your data generator
+    #data = SemDataGenerator(config)
+    #data = MSRPGenerator(config)
+    data = ATECGenerator(config)
+
+    if args.step == "build_data":
+        print("build data....")
+        data.build_data()
+    elif args.step == "train":
+        # create the experiments dirs
+        create_dirs([config.summary_dir, config.checkpoint_dir])
+        # create tensorflow session
+        sess = tf.Session()
+    
+        # load word2vec
+        config.embedding = data.get_trimmed_glove_vectors() 
+
+        model = ESIM(config)
+        # create tensorboard logger
+        logger = Logger(sess, config)
+        # create trainer and pass all the previous components to it
+        trainer = SentSemTrainer(sess, model, data, config, logger)
+        #load model if exists
+        #model.load(sess)
+        # here you train your model
+        trainer.train()
+    elif args.step == "tune":
+
+        import itertools
+        tune_num = 0
+        param_names = config["parameter_tune"].keys()
+        print(param_names)
+        cand_params = [config["parameter_tune"][pname] for pname in param_names]
+        print(cand_params)
+        for params in itertools.product(*cand_params):
+            print(params)
+            for i, param_name in enumerate(param_names):
+                config[param_name] = params[i]
+            #print(config)
+            data = ATECGenerator(config)
+            create_dirs([config.summary_dir, config.checkpoint_dir])
+            sess = tf.Session()
+            config.embedding = data.get_trimmed_glove_vectors() 
+            model = CDSSM(config)
+            logger = Logger(sess, config)
+            trainer = SentSemTrainer(sess, model, data, config, logger)
+            trainer.train()
+            tf.reset_default_graph()
+    else:
+        print("no support step!!")
+
+if __name__ == '__main__':
+    main()
---
+++ models/blocks.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from tensorflow.contrib.rnn import LSTMCell, DropoutWrapper
+
+def _feedforward_block(inputs, hidden_dims, num_units, scope, dropout_keep_prob=1.0, reuse = False, initializer = None):
+    """
+        :param inputs: tensor with shape (batch_size, hidden_size)
+        :param scope: scope name
+        :return: output: tensor with shape (batch_size, num_units)
+    """
+    with tf.variable_scope(scope, reuse=reuse):
+        if initializer is None:
+            initializer = tf.random_normal_initializer(0.0, 0.1)
+
+        with tf.variable_scope('feed_foward_layer1'):
+            inputs = tf.nn.dropout(inputs, dropout_keep_prob)
+            outputs = tf.layers.dense(inputs, hidden_dims, tf.nn.relu, kernel_initializer = initializer)
+        with tf.variable_scope('feed_foward_layer2'):
+            outputs = tf.nn.dropout(outputs, dropout_keep_prob)
+            results = tf.layers.dense(outputs, num_units, tf.nn.tanh, kernel_initializer = initializer)
+        return results
+
+
+def _bilstm_block(inputs, num_units, scope, dropout_keep_prob=1.0, seq_len=None, reuse=False):
+    """
+       :param inputs: tensor with shape (batch_size, hidden_size)
+       :param scope: scope name
+       :output:  tuple (outputs, output_states) where outputs is a tuple (output_fw, output_bw)
+    """
+    with tf.variable_scope(scope, reuse=reuse):
+        # reuse lstm cell for fw and bw cell
+        lstm_cell = LSTMCell(num_units=num_units)
+        lstmDrop = lambda: DropoutWrapper(lstm_cell, output_keep_prob=dropout_keep_prob)
+        lstm_cell_fw, lstm_cell_bw = lstmDrop(), lstmDrop()
+        output = tf.nn.bidirectional_dynamic_rnn(cell_fw = lstm_cell_fw,
+                                                 cell_bw = lstm_cell_bw,
+                                                 inputs = inputs,
+                                                 sequence_length = seq_len,
+                                                 dtype = tf.float32)
+        return output
+
